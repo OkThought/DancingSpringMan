4bdd53f30a3ecc1d2c310ba8f9715f1761f7643e
Ivan Bogush <bogush.vano@gmail.com>
2019-09-15 00:59:08 +0700

    Add a following circle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { useSpring, animated } from "react-spring";
 
 import "./styles.css";
 
 function App() {
-  const [name, setName] = useState("John");
+  const [{ left, top }, set] = useState({
+    left: 0,
+    top: 0
+  });
+  const style = useSpring({
+    left,
+    top,
+    to: { left, top },
+    from: { left: 0, top: 0 }
+  });
   return (
-    <div className="App">
-      <input onChange={({ value }) => setName(value)} value={name} />
+    <div
+      className="App"
+      onMouseMove={e => {
+        set({ left: e.clientX, top: e.clientY });
+      }}
+    >
+      <animated.div className="circle" style={style} />
     </div>
   );
 }
